2aaa383f3ba6b3f1f09746c75a892fd627922ae5
astrobolidos <lisandroalavez@gmail.com>
2015-03-15 15:46:43 +0000

    initial files

--- webapi2-react-authentication/public/js/app.js
+++ webapi2-react-authentication/public/js/app.js
@@ -1,7 +1,7 @@
 window.React  = require('react/addons');
 var React     = require('react/addons');
 
-var App = React.createClass({
+var MyApp = React.createClass({
     render: function() {
         return (
             <h1>Welcome!</h1>
@@ -10,4 +10,4 @@ var App = React.createClass({
 }); 
 
 
-React.render(<App />, document.body);
+React.render(<MyApp />, document.body);
